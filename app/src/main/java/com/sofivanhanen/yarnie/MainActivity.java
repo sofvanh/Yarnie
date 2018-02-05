21ee914b568f86ca2bc848555d62887958fea49c
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-02-05 18:11:04 +0200

    First layout
    Base of final layout for testing purposes

--- app/src/main/java/com/sofivanhanen/yarnie/MainActivity.java
+++ app/src/main/java/com/sofivanhanen/yarnie/MainActivity.java
@@ -2,6 +2,10 @@ package com.sofivanhanen.yarnie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
     public void makeToast(String text) {
         Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void handleClick(View v) {
+        if (v.getId() == R.id.button_search) {
+            TextView tv = findViewById(R.id.tv_pattern_result);
+            EditText et = findViewById(R.id.et_amount_of_yarn);
+            tv.clearAnimation();
+            tv.setText(tv.getText() + "\n" + "Button clicked! Text was: " + et.getText());
+            tv.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+        }
     }
 }
