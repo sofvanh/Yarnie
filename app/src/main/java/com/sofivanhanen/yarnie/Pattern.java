295d7df42e6a27f7dd5141ca00de90e6415cee66
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-02-09 22:18:36 +0200

    New parameters for Pattern

--- app/src/main/java/com/sofivanhanen/yarnie/Pattern.java
+++ app/src/main/java/com/sofivanhanen/yarnie/Pattern.java
@@ -9,12 +9,17 @@ public class Pattern {
     private boolean free;
     private int id;
     private String name;
-    // TODO: More parameters! Yardage esp.
+    private int yardage;
+    private int yardage_max;
 
     public boolean getFree() { return free; }
 
     public int getId() { return id; }
 
-    public String getName() { return name; };
+    public String getName() { return name; }
+
+    public int getYardage() { return yardage; }
+
+    public int getYardage_max() { return yardage_max; }
 
 }
