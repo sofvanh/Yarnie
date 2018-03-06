9a98c6a99d98f184db1c1bc7ea018901f0d6df0a
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-03-06 10:26:10 +0200

    New values for Pattern

--- app/src/main/java/com/sofivanhanen/yarnie/Data/Pattern.java
+++ app/src/main/java/com/sofivanhanen/yarnie/Data/Pattern.java
@@ -1,5 +1,7 @@
 package com.sofivanhanen.yarnie.Data;
 
+import java.util.Date;
+
 /**
  * Created by sofvanh on 02/02/18.
  */
@@ -9,7 +11,11 @@ public class Pattern {
     private boolean free;
     private int id;
     private String name;
+    private int projects_count;
+    private Date published;
     private int yardage;
+
+    private double value;
 
     // Setters for testing purposes
     public void setFree(boolean free) { this.free = free; }
@@ -18,10 +24,21 @@ public class Pattern {
     public void setId(int id) { this.id = id; }
     public int getId() { return id; }
 
-    public void setName(String name) { this.name = name; };
+    public void setName(String name) { this.name = name; }
     public String getName() { return name; }
+
+    public void setProjects_count(int id) { this.projects_count = projects_count; }
+    public int getProjects_count() { return projects_count; }
+
+    public void setPublished(Date date) { this.published = date; }
+    public Date getPublished() { return published; };
 
     public void setYardage(int yardage) { this.yardage = yardage; }
     public int getYardage() { return yardage; }
+
+    public double getValue() {
+        return 1;
+        // TODO: If value not calculated, calculate it here, save, and return
+    }
 
 }
