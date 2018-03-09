0f06220f5ca8f6bdc35c69d1c006f4198b3a0ad3
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-03-09 21:43:47 +0200

    Final documentation
    Generated javadoc & test coverage, finished test, run, and implementing documents

--- app/src/test/java/com/sofivanhanen/yarnie/PerformanceTest.java
+++ app/src/test/java/com/sofivanhanen/yarnie/PerformanceTest.java
@@ -14,9 +14,54 @@ import java.util.Random;
 public class PerformanceTest {
 
     @Test
+    public void patternKnapsackWeightOnly_performance_size20_100times() {
+        for (int i = 0; i < 100; i++) {
+            patternKnapsackWeightOnly_performance_size20();
+        }
+    }
+
+    @Test
+    public void patternKnapsackWeightOnly_performance_size35_75times() {
+        for (int i = 0; i < 75; i++) {
+            patternKnapsackWeightOnly_performance_size35();
+        }
+    }
+
+    @Test
+    public void patternKnapsackWeightOnly_performance_size50_50times() {
+        for (int i = 0; i < 50; i++) {
+            patternKnapsackWeightOnly_performance_size50();
+        }
+    }
+
+    @Test
+    public void patternKnapsack_performance_size20_100times() {
+        for (int i = 0; i < 100; i++) {
+            patternKnapsack_performance_size20();
+        }
+    }
+
+    @Test
+    public void patternKnapsack_performance_size35_75times() {
+        for (int i = 0; i < 75; i++) {
+            patternKnapsack_performance_size35();
+        }
+    }
+
+    @Test
+    public void patternKnapsack_performance_size50_50times() {
+        for (int i = 0; i < 50; i++) {
+            patternKnapsack_performance_size50();
+        }
+    }
+
+    @Test
     public void patternKnapsackWeightOnly_performance_size20() {
         runPatternKnapsackWeightOnly(20);
     }
+
+    @Test
+    public void patternKnapsackWeightOnly_performance_size35() { runPatternKnapsackWeightOnly(35); }
 
     @Test
     public void patternKnapsackWeightOnly_performance_size50() {
@@ -28,11 +73,28 @@ public class PerformanceTest {
         runPatternKnapsackWeightOnly(100);
     }
 */
+
+    @Test
+    public void patternKnapsack_performance_size20() { runPatternKnapsack(20); }
+
+    @Test
+    public void patternKnapsack_performance_size35() { runPatternKnapsack(35); }
+
+    @Test
+    public void patternKnapsack_performance_size50() { runPatternKnapsack(50); }
+
     private void runPatternKnapsackWeightOnly(int n) {
         int sizeOfArray = n;
         int maxYardage = n * 10;
         int maxYardageOfPattern = maxYardage / 2;
         AlgoUtils.patternKnapsackWeightOnly(createRandomArray(n, maxYardageOfPattern), maxYardage);
+    }
+
+    private void runPatternKnapsack(int n) {
+        int sizeOfArray = n;
+        int maxYardage = n * 10;
+        int maxYardageOfPattern = maxYardage / 2;
+        AlgoUtils.patternKnapsack(createRandomArrayWithValues(n, maxYardageOfPattern), maxYardage);
     }
 
     private Pattern[] createRandomArray(int size, int maxYardageOfPattern) {
@@ -41,6 +103,18 @@ public class PerformanceTest {
         for (int i = 0; i < size; i++) {
             Pattern pattern = new Pattern();
             pattern.setYardage(random.nextInt(maxYardageOfPattern));
+            result[i] = pattern;
+        }
+        return result;
+    }
+
+    private Pattern[] createRandomArrayWithValues(int size, int maxYardageOfPattern) {
+        Pattern[] result = new Pattern[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            Pattern pattern = new Pattern();
+            pattern.setYardage(random.nextInt(maxYardageOfPattern));
+            pattern.setFree(random.nextInt(2) == 1);
             result[i] = pattern;
         }
         return result;
